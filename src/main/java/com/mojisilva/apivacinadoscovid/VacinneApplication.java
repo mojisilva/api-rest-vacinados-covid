24adec4f3a6066003ae65a4c26b296911b115e61
mojisilva <mojiasilva@gmail.com>
2022-02-27 15:13:14 -0300

    Criação do projeto
    Este commit se refere a criação da api de gerencimanento de vacinados covid utilizando api rest.

---
+++ src/main/java/com/mojisilva/apivacinadoscovid/VacinneApplication.java
@@ -0,0 +1,13 @@
+package com.mojisilva.apivacinadoscovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacinneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacinneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mojisilva/apivacinadoscovid/VacinneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mojisilva.apivacinadoscovid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacinneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
