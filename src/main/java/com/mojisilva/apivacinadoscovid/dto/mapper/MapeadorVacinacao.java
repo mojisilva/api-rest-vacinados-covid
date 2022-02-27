50b1ac048a3cb3eb442792c299346cc8962f06b7
mojisilva <mojiasilva@gmail.com>
2022-02-27 20:41:00 -0300

    alterações no projeto

--- src/main/java/com/mojisilva/apivacinadoscovid/dto/mapper/MapeadorVacinacao.java
+++ src/main/java/com/mojisilva/apivacinadoscovid/dto/mapper/MapeadorVacinacao.java
@@ -1,9 +1,7 @@
 package com.mojisilva.apivacinadoscovid.dto.mapper;
 
 import com.mojisilva.apivacinadoscovid.dto.request.PacienteDTO;
-import com.mojisilva.apivacinadoscovid.dto.request.VacinaDTO;
 import com.mojisilva.apivacinadoscovid.entity.Paciente;
-import com.mojisilva.apivacinadoscovid.entity.Vacina;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -12,11 +10,6 @@ public interface MapeadorVacinacao {
 
     @Mapping(target = "aniversario", source = "aniversario", dateFormat = "dd-MM-yyyy")
     Paciente toModel(PacienteDTO dto);
-
     PacienteDTO toDTO(Paciente dto);
 
-    @Mapping(target = "dataVacinacao", source = "dataVacinacao", dateFormat = "dd-MM-yyyy")
-    Vacina toModel(VacinaDTO dto);
-
-    VacinaDTO toDTO(VacinaDTO dto);
 }
--- src/main/java/com/mojisilva/apivacinadoscovid/services/ServicoVacinacao.java
+++ src/main/java/com/mojisilva/apivacinadoscovid/services/ServicoVacinacao.java
@@ -1,4 +1,33 @@
 package com.mojisilva.apivacinadoscovid.services;
 
+import com.mojisilva.apivacinadoscovid.dto.mapper.MapeadorVacinacao;
+import com.mojisilva.apivacinadoscovid.dto.request.PacienteDTO;
+import com.mojisilva.apivacinadoscovid.dto.response.MensagemRespostaDTO;
+import com.mojisilva.apivacinadoscovid.entity.Paciente;
+import com.mojisilva.apivacinadoscovid.repository.RepositorioVacinacao;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class ServicoVacinacao {
+
+    private final RepositorioVacinacao repositorioVacinacao;
+    private final MapeadorVacinacao mapeadorVacinacao;
+
+
+
+   /* @PostMapping
+    public MensagemRespostaDTO criarPaciente(@RequestBody PacienteDTO pacienteDTO) {
+        Paciente paciente = mapeadorVacinacao.toModel(pacienteDTO);
+        Paciente pacienteSalvo = repositorioVacinacao.save(paciente);
+        return MensagemRespostaDTO
+                .builder()
+                .mensagem("Resgistro do paciente salvo com sucesso ID " + pacienteSalvo.getId())
+                .build();
+    }*/
 }
